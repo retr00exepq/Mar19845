3065216aadfe748f5925aed5b166c5b4ada59d10
retr00exepq <a838221222@sina.com>
2021-02-15 19:37:25 +0800

    Carrousel, Producto

--- AppWeb_Proyecto/bodega502/src/componentes/Producto/Producto.js
+++ AppWeb_Proyecto/bodega502/src/componentes/Producto/Producto.js
@@ -2,12 +2,42 @@
 import './Producto.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import sillon from './sillonPrueba.jpg';
+import { Container } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Carousel from './Carrousel';
 
 export default function Producto(){
     return(    
-    <div id="Producto">
-        <img src={sillon} alt="Sillon" className="rounded float-left"></img>
-    </div>
+        <Container fluid>
+            <Row >
+                <Col xs={1} sm={1} md={1} lg={1} className="Ang">
+                    <img src={sillon} alt="Sillon"></img>
+                    <img src={sillon} alt="Sillon"></img>
+                    <img src={sillon} alt="Sillon"></img>
+                    <img src={sillon} alt="Sillon"></img>
+                </Col>
+
+                <Col xs={5} sm={5} md={5} lg={5} className="Ima">
+                    <Carousel></Carousel>
+                </Col>
+
+                <Col xs={6} sm={6} md={6} lg={6} className="Des">
+                    <p>Nombre:<br></br>
+                    Precio:<br></br>
+                    <details>
+                        <summary>Descripción</summary><br></br>
+                    </details>
+                    <details>
+                        <summary>Detalles tecnicos</summary><br></br>
+                    </details>
+                    </p>
+                </Col>
+            </Row>
+            <Row xs={12} sm={12} md={12} lg={12}>
+                <Col xs={12} sm={12} md={12} lg={12} className="Rec">recomendaciones</Col>
+            </Row>
+        </Container>
     );
 }
 
