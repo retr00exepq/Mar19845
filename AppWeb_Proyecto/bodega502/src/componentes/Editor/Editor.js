1c6c1bb0f4169df925752818d55934208065b6de
retr00exepq <a838221222@sina.com>
2021-03-06 02:40:06 +0800

    Cambio de estilo

--- AppWeb_Proyecto/bodega502/src/componentes/Editor/Editor.js
+++ AppWeb_Proyecto/bodega502/src/componentes/Editor/Editor.js
@@ -33,10 +33,8 @@ class Editor extends Component {
       <div className="main_div">
         <div className="grid_row">
           <div>
-            <label className="title_label" style={{marginRight: "20px"}}>
-                <text>Nombre del producto:</text>
-                <input type="text" name="name" maxLength={50} className="title_input"/>
-            </label>
+              <text>Nombre del producto:</text>
+              <input type="text" name="name" maxLength={50} className="title_input"/>
           </div>
           <div>
             <text>Seleccione el tipo de producto</text>
@@ -52,23 +50,31 @@ class Editor extends Component {
             />
           </div>
         </div>
-        <div className="grid_row" style={{marginTop: "100px"}}>
-          <div>
-            <label className="price_label">
+        <div className="grid_row" style={{marginTop: "100px", height: "250px"}}>
+          <div className="grid_column">
               <text>Precio del producto (Q):</text>
-              <input type="text" style={{marginRight:"10px"}} name="name" maxLength={50} className="number_input" value={this.state.value1} onChange={this.onChange1}/>
+              <input type="text" name="name" maxLength={50} className="number_input" value={this.state.value1} onChange={this.onChange1}/>
               <text>Cantidad en existencia:</text>
-              <input type="text" style={{marginRight:"10px"}} name="name" maxLength={50} className="number_input" value={this.state.value2} onChange={this.onChange2}/>
-              <text>% de Descuento:</text>
-              <input type="text" name="porcentage_general" maxLength={50} />
-            </label>
+              <input type="text" name="name" maxLength={50} className="number_input" value={this.state.value2} onChange={this.onChange2}/>
+          </div>
+          <div className="grid_column">
+              <text>% de descuento con tarjeta:</text>
+              <input type="text" name="name" maxLength={50} className="number_input" value={this.state.value1} onChange={this.onChange1}/>
+              <text>Nuevo precio:</text>    
+              <text>0</text>    
+          </div>
+          <div className="grid_column">
+              <text>% de descuento por cuotas:</text>
+              <input type="text" name="name" maxLength={50} className="number_input" value={this.state.value1} onChange={this.onChange1}/>
+              <text>Nuevo precio:</text>    
+              <text>0</text>    
           </div>
         </div>
-        <div className="grid_row_p" style={{marginTop: "200px", gridAutoFlow: "row"}}>
+        <div className="grid_row_p" style={{marginTop: "400px", gridAutoFlow: "row"}}>
           <text>Descripcion del producto</text>
           <textarea className="description" type="text" name="name" maxLength={500}/>
         </div>
-        <div className="grid_row" style={{marginTop: "800px", left: "26%", gap: "10rem"}}>
+        <div className="grid_row" style={{marginTop: "1000px", left: "26%", gap: "10rem"}}>
             <button className='guardar_button' type="button">Guardar Cambios</button>
             <button className='eliminar_button' type="button">Eliminar Publicacion</button>
         </div>
