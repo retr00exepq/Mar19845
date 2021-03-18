ad506cc71c6793d58b310563c0987e1c7ac11790
retr00exepq <a838221222@sina.com>
2021-03-19 07:12:32 +0800

    odoo.js

---
+++ AppWeb_Proyecto/bodega502/src/oddo.js
@@ -0,0 +1,15 @@
+const  Odoo = require('odoo-xmlrpc')
+
+const odoo = new Odoo({
+  url: 'https://proyecto10.odoo.com/',
+  port: 8080, /* Defaults to 80 if not specified */
+  db: 'proyecto10',
+  username : 'cot19324@uvg.edu.gt', /* Optional Like Change Language */
+  password: 'ProyectoSoft123'
+})
+console.log('funciona esto????')
+
+odoo.connect(function (err) {
+    if (err) { return console.log(err); }
+    console.log('Connected to Odoo server.');    
+});
