0c7a99e556ecbbefa451f123da0cdab216b44c73
retr00exepq <a838221222@sina.com>
2021-03-22 09:19:21 +0800

    Update oddo.js

--- AppWeb_Proyecto/bodega502/src/oddo.js
+++ AppWeb_Proyecto/bodega502/src/oddo.js
@@ -1,11 +1,11 @@
 const  Odoo = require('odoo-xmlrpc')
 
 const odoo = new Odoo({
-  url: 'https://proyecto10.odoo.com/',
-  port: 8080, /* Defaults to 80 if not specified */
+  url: 'https://proyecto10.odoo.com',
+  //port: 80, /* Defaults to 80 if not specified */
   db: 'proyecto10',
   username : 'cot19324@uvg.edu.gt', /* Optional Like Change Language */
-  password: 'ProyectoSoft123'
+  password: 'ProyectoSoft1234'
 })
 console.log('funciona esto????')
 
