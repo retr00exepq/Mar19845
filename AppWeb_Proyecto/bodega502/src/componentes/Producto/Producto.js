8bcc854c269e9ece5c7248b71642e39b2c322517
retr00exepq <a838221222@sina.com>
2021-04-12 01:48:30 +0800

    Modulo Producto Actualizado

--- AppWeb_Proyecto/bodega502/src/componentes/Producto/Producto.js
+++ AppWeb_Proyecto/bodega502/src/componentes/Producto/Producto.js
@@ -11,17 +11,9 @@ export default function Producto(){
     return(    
         <Container fluid>
             <Row >
-                <Col xs={1} sm={1} md={1} lg={1} className="Ang">
-                    <img src={sillon} alt="Sillon"></img>
-                    <img src={sillon} alt="Sillon"></img>
-                    <img src={sillon} alt="Sillon"></img>
-                    <img src={sillon} alt="Sillon"></img>
-                </Col>
-
-                <Col xs={5} sm={5} md={5} lg={5} className="Ima">
+                <Col xs={6} sm={6} md={6} lg={6} className="Ima">
                     <Carousel></Carousel>
                 </Col>
-
                 <Col xs={6} sm={6} md={6} lg={6} className="Des">
                     <p>Nombre:<br></br>
                     Precio:<br></br>
@@ -29,7 +21,7 @@ export default function Producto(){
                         <summary>Descripción</summary><br></br>
                     </details>
                     <details>
-                        <summary>Detalles tecnicos</summary><br></br>
+                        <summary>Detalles Técnicos</summary><br></br>
                     </details>
                     </p>
                 </Col>
