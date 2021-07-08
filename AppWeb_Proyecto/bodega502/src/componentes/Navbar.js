0402fc25fef7b798d063560ee01ba9263c5a950d
retr00exepq <a838221222@sina.com>
2021-07-08 15:05:29 +0800

    push

--- AppWeb_Proyecto/bodega502/src/componentes/Navbar.js
+++ AppWeb_Proyecto/bodega502/src/componentes/Navbar.js
@@ -1,59 +1,37 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+
 import bodega from '../imagenes/bodega502.jpg';
 import './Navbar.css';
-import { FaSistrix } from 'react-icons/fa';
-import { FaUserAlt } from 'react-icons/fa';
-import React, { Component } from "react";
+import { AppBar, Toolbar, IconButton, Typography,InputBase,Paper,Divider} from '@mui/material';
 import { Link } from "react-router-dom";
-
-//FaRegCircle
-function NavBar() {
-  return (
-    <Navbar bg="dark" expand="lg">
-      <Navbar.Brand >
-        <Link to='/'>
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+/*
+<Link to='/'>
           <img src={bodega} alt='Bodega 502' className='logo'></img>
         </Link>
-        
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown title="Menu" id="basic-nav-dropdown" className="MenuDrop" >
-            <NavDropdown.Item >
-              <Link to='/oferta'>
-                Ofertas
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item >
-              <Link to='/editor'>
-                Editor
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item >
-              <Link to='/producto'>
-                producto
-              </Link>
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
+*/
 
-        <Form inline>
-          <FormControl type="text" placeholder="Buscar..." className="mr-sm-2" />
-          <Button variant="primary">
-            <FaSistrix className="SearchBtn" />
-          </Button>
-        </Form>
-
-
-
-      </Navbar.Collapse>
-    </Navbar>
+function NavBar() {
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          size="small"
+          edge="false"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <Link to='/'>
+            <img src={bodega} alt='Bodega 502' className='logo'></img>
+          </Link>
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+            La Bodega 502
+        </Typography>
+        <AccountCircle/>
+      </Toolbar>
+    </AppBar>
   );
 }
 
