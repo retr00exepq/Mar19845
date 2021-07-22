fa9cccfde857b02bd10b6b5eb864bea7525ce17e
retr00exepq <a838221222@sina.com>
2021-07-22 21:51:59 +0800

    User creation and recognition

--- AppWeb_Proyecto/bodega502/server.js
+++ AppWeb_Proyecto/bodega502/server.js
@@ -136,32 +136,160 @@ app.post('/delete',(req,res)=>{
     })
 })
 
+// app.post('/pedido',(req,res)=>{
+//     let id = req.body.id;
+//     let price = req.body.list_price
+//     let vals = {
+//         'pricelist_id': 1,
+//         'partner_id': 8,
+//         'session_id': 1,
+//         'amount_tax': 2 * price,
+//         'amount_total': 2 * price + 100,
+//         'amount_paid': 2 * price + 100,
+//         'amount_return': 0,
+//     }
+//     odoo.connect(function (err) {
+//         if (err) { return console.log(err); }
+//         var inParams = [];
+//         inParams.push([vals])
+//         var params = [];
+//         params.push(inParams);
+//         odoo.execute_kw('pos.order', 'create', params, function (err, value) {
+//             if (err) { return console.log(err); }
+//             res.send(value)
+//         });
+//     })
+// })
+
+// app.post('/pedido',(req,res)=>{
+//     let id = req.body.id;
+//     let price = req.body.list_price
+//     let vals = {
+//         'price_subtotal': 100,
+//         'price_subtotal_incl':100,
+//         'product_id':(0, 0, {'id':id}),
+//         'name': "ff",
+//         'order_id':(0, 0, {'id':1})
+//     }
+//     let line = (0, 0, vals)
+//     let vals1 = {
+//         'pricelist_id': 1,
+//         'partner_id': 8,
+//         'session_id': 1,
+//         'amount_tax': 2 * price,
+//         'amount_total': 2 * price + 100,
+//         'amount_paid': 2 * price + 100,
+//         'amount_return': 0,
+//         'id': 10,
+//         'lines': line
+//     }
+//     // odoo.connect(function (err) {
+//     //     if (err) { return console.log(err); }
+//     //     var inParams = [];
+//     //     inParams.push([vals])
+//     //     var params = [];
+//     //     params.push(inParams);
+//     //     odoo.execute_kw('pos.order.line', 'create', params, function (err, value) {
+//     //         if (err) { return console.log(err); }
+//     //         res.send(value)
+//     //     });
+//     // })
+//     odoo.connect(function (err) {
+//         if (err) { return console.log(err); }
+//         var inParams = [];
+//         inParams.push([vals1])
+//         var params = [];
+//         params.push(inParams);
+//         odoo.execute_kw('pos.order', 'create', params, function (err, value) {
+//             if (err) { return console.log(err); }
+//             res.send(value)
+//         });
+//     })
+// })
+
 app.post('/pedido',(req,res)=>{
-    let id = req.body.id;
     let price = req.body.list_price
-    let vals = {
-        'pricelist_id': 1,
-        'partner_id': 8,
-        'session_id': 1,
-        'amount_tax': 2 * price,
-        'amount_total': 2 * price + 100,
-        'amount_paid': 2 * price + 100,
-        'amount_return': 0,
+    let user_name = req.body.user_name
+    let user_mobile = req.body.user_mobile
+    let user_email = req.body.user_email
+    let user_adress = req.body.user_adress
+
+    let user_vals = {
+        'email': user_email,
+        'email_formatted': user_email,
+        'email_normalized': user_email,
+        'contact_address': user_adress,
+        'street': user_adress,
+        'name': user_name,
+        'display_name': user_name,
+        'mobile': user_mobile,
+        'phone': user_mobile,
+        'customer_rank': 1
     }
+    
     odoo.connect(function (err) {
         if (err) { return console.log(err); }
         var inParams = [];
-        inParams.push([vals])
+        inParams.push([['email', '=', user_email]])
         var params = [];
         params.push(inParams);
-        odoo.execute_kw('pos.order', 'create', params, function (err, value) {
+        odoo.execute_kw('res.partner', 'search', params, function (err, value) {
             if (err) { return console.log(err); }
-            res.send(value)
+        
+            if (value.length === 0){
+                var inParams = [];
+                inParams.push(user_vals)
+                var params = [];
+                params.push(inParams);
+                odoo.execute_kw('res.partner', 'create', params, function (err, value) {
+                    if (err) { return console.log(err); }
+
+                    var inParams = [];
+                    inParams.push([['email', '=', user_email]])
+                    var params = [];
+                    params.push(inParams);
+                    odoo.execute_kw('res.partner', 'search', params, function (err, value) {
+                        if (err) { return console.log(err); }
+                        
+                        let id_user = value[0]
+                        inParams = [];
+                        inParams.push({
+                            'pricelist_id': 1,
+                            'session_id': 1,
+                            'amount_tax': 2 * price,
+                            'amount_total': 2 * price + 100,
+                            'amount_paid': 2 * price + 100,
+                            'amount_return': 0,
+                            'partner_id': id_user
+                        })
+                        params = [];
+                        params.push(inParams);
+                        odoo.execute_kw('pos.order', 'create', params, function (err, value) {
+                            if (err) { return console.log(err); }
+                        });
+                    });
+                });
+            }else{
+                let id_user = value[0]
+                inParams = [];
+                inParams.push({
+                    'pricelist_id': 1,
+                    'session_id': 1,
+                    'amount_tax': 2 * price,
+                    'amount_total': 2 * price + 100,
+                    'amount_paid': 2 * price + 100,
+                    'amount_return': 0,
+                    'partner_id': id_user
+                })
+                params = [];
+                params.push(inParams);
+                odoo.execute_kw('pos.order', 'create', params, function (err, value) {
+                    if (err) { return console.log(err); }
+                });
+            }
         });
     })
 })
-
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- AppWeb_Proyecto/bodega502/src/componentes/Compra/Compra.js
+++ AppWeb_Proyecto/bodega502/src/componentes/Compra/Compra.js
@@ -19,16 +19,46 @@ export default function Compra({ match }) {
   });
 
   async function venta() {
-    await fetch('http://localhost:3001/pedido',{method: "POST", 
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({"id": parseInt(match.params.id),
-                          "list_price": parseFloat(product.list_price),    
+    console.log(userName)
+    if (userName.length === 0 || userPhone.length === 0 || userMail.length === 0 || userAdress.length === 0){
+      alert('Debe llenar todos los campos')
+    }else{
+      await fetch('http://localhost:3001/pedido',{method: "POST", 
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({"id": parseInt(match.params.id),
+                          "list_price": parseFloat(product.list_price),
+                          "user_name": userName,
+                          "user_mobile": userPhone,
+                          "user_email": userMail,
+                          "user_adress": userAdress,    
                         })})
         .then(response => response.json())
         .then(data =>{
             SetProduct(data[0])          
         })
+    }
   }
+
+  const [userName, setUserName] = useState('')
+  const [userPhone, setUserPhone] = useState('')
+  const [userMail, setUserMail] = useState('')
+  const [userAdress, setUserAdress] = useState('')
+
+  const onChangeUserName = event => {
+    setUserName( event.target.value );
+  };
+
+  const onChangeUserPhone = event => {
+    setUserPhone( event.target.value );
+  };
+
+  const onChangeUserMail = event => {
+    setUserMail( event.target.value );
+  };
+
+  const onChangeUserAdress = event => {
+    setUserAdress( event.target.value );
+  };
 
     return (
       <div className="main_div_compra">
@@ -45,6 +75,25 @@ export default function Compra({ match }) {
             <div>
                 <text>Descripcion: {product.description}</text>
             </div>
+          </div>
+        </div>
+        <div style={{marginTop: "10px"}}>
+          <text style={{textAlign: 'center'}}>Sus datos</text>
+          <div>
+              <text>Nombre: </text>
+              <input type="text" name="name" maxLength={100} onChange={onChangeUserName}/>
+          </div>
+          <div>
+              <text>Telefono: </text>
+              <input type="text" name="name" maxLength={100} onChange={onChangeUserPhone}/>
+          </div>
+          <div>
+              <text>Correo:</text>
+              <input type="text" name="name" maxLength={100} onChange={onChangeUserMail}/>
+          </div>
+          <div>
+              <text>Direccion:</text>
+              <input type="text" name="name" maxLength={100} onChange={onChangeUserAdress}/>
           </div>
         </div>
         <div className="compra_grid_row" style={{marginTop: "400px", left: "26%", gap: "10rem"}}>
