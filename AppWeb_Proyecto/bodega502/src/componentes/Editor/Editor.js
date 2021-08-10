0c8d880a361b7abfe5abf52bfd395b093cef1522
retr00exepq <a838221222@sina.com>
2021-08-10 20:38:39 +0800

    Revert "Merge branch 'main' of https://github.com/Mar19845/Bodega502-Proyecto-Ing-Software"
    This reverts commit da6c304821d89f71a5e537ca21525bcf9196aae5, reversing
    changes made to 7f0b8796058850f40ad7313fc826d397205e17a7.

--- AppWeb_Proyecto/bodega502/src/componentes/Editor/Editor.js
+++ AppWeb_Proyecto/bodega502/src/componentes/Editor/Editor.js
@@ -2,13 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Select from 'react-select';
 import "./Editor_style.css";
-import Grid from '@material-ui/core/Grid';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button';
-import {makeStyles} from '@material-ui/core'
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
 
 export default function Editor({ match }) { 
 
@@ -79,74 +72,7 @@ export default function Editor({ match }) {
     setValue3( event.target.value );
   };
 
-  /*const useStyles = makeStyles({
-    stack1:{
-      position: "absolute",
-      marginTop: 400,
-      marginLeft:100
-    },
-    stack2:{
-      position: "absolute",
-      marginTop: 400,
-      marginLeft:360
-    },
-  })*/
-  /*<Stack className={classes.stack1} spacing={4}>*/ 
-  //const classes = useStyles()
-    return(
-      <Grid container spacing={2} xs={12} sm={6} md={3}>
-        <div className ='texto'>
-          <Grid item>
-            <Stack spacing={6}>
-                <Typography variant = "h5" component = "div" >Nombre del Producto: </Typography>  
-                <Typography variant = "h5" component = "div" >Precio del Producto:</Typography>
-                <Typography variant = "h5" component = "div" >Cantidad en Existencia: </Typography>  
-                <Typography variant = "h5" component = "div" >Seleccione el tipo de producto: </Typography>  
-            </Stack>
-            </Grid>
-        </div>
-        <div className ='campos'>
-        <Grid item>
-            <Stack spacing={6} item md = {6}>
-            <input type="text" name="name" maxLength={100} className="title_input" defaultValue={product.display_name} onChange={onChange0}/>
-            <input type="number" maxLength={50} className="number_input" defaultValue={product.list_price} onChange={onChange1}/> 
-            <input type="number" name="name" maxLength={50} className="number_input" defaultValue={product.qty_available} onChange={onChange2}/>
-            <Select
-              className="basic-single"
-              classNamePrefix="select"
-              defaultValue={options[0]}
-              isSearchable={true}
-              name="product-type"
-              options={options}
-            />
-            </Stack>
-            </Grid>
-        </div>
-
-        <div className="desc">
-        <Grid item>
-          <Stack spacing={6}>
-          <Typography variant = "h5" component = "div" >Descripcion del producto: </Typography>  
-          <textarea className="description" type="text" name="name" maxLength={500} defaultValue={product.description} onChange={onChange3}/>
-          </Stack>
-          </Grid>
-        </div>
-
-        <div className="botones">
-        <Grid container spacing={6}>
-          <Grid item>
-        <button className='guardar_button' type="button" onClick={() => {actualizar()}}>Guardar Cambios</button>
-        </Grid>
-        <Grid item>
-        <button className='eliminar_button' type="button" onClick={() => {eliminar()}}>Eliminar Publicacion</button>
-        </Grid>
-          </Grid>
-        </div>
-
-      </Grid>
-    )
-
-    /*return (
+    return (
       <div className="main_div" data-testid="existe1">
         <div className="grid_row">
           <div>
@@ -188,5 +114,5 @@ export default function Editor({ match }) {
           </Link>
         </div>
       </div>
-    );*/
+    );
 } 
